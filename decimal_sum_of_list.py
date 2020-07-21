3f425c4366a00db53b93913e57c7b98cfd9f5e94
CreativeCloudAppDev2020 <63490399+CreativeCloudAppDev2020@users.noreply.github.com>
2020-07-21 19:44:17 +1200

    Create decimal_sum_of_list.py

---
+++ decimal_sum_of_list.py
@@ -0,0 +1,37 @@
+# Calculating the Decimal sum of elements in a list which will work for any types of list
+
+
+from decimal import Decimal  # importing necessary library
+
+
+# Checking whether a string is a number open
+
+
+def is_number(string: str) -> bool:
+  try:
+    Decimal(string)
+    return True
+  except:
+    return False
+
+
+def decimal_sum_of_list(a_list: list) -> Decimal:
+  sum: Decimal = Decimal("0")  # initial value
+  for elem in a_list:
+    try:
+      # Converting 'elem' into a string at first
+      str_elem: str = str(elem)
+      # Checking whether str_elem is a number or not
+      if is_number(str_elem):
+        # Add 'sum' by the Decimal value of str_elem
+        sum += Decimal(str_elem)
+    except:
+      pass  # Adding code just so that no errors are thrown
+
+  return sum
+
+
+# Testing the function
+
+print(decimal_sum_of_list([4, 3, "5.17", "3.22s"]))  # 12.17
+print(decimal_sum_of_list(["4.132", "5435.34243254325", "3234324.321432153425", "ssd", "xwt", 5]))  # 3239768.795864696675
